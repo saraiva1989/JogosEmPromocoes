987051ce47d52de69df31b4a3801e9aa54ebb6ac
Daniel Saraiva <danniel.saraiva@gmail.com>
2021-11-14 14:42:09 -0300

    Ajustes e melhorias

---
+++ js/environment.js
@@ -0,0 +1,19 @@
+var urlAPIJogos = ""
+const arrayHostDev = ["jogosempromocoesdev.netlify.app", "localhost:5500", "127.0.0.1:5500"]
+const arrayHostProd = ["saraiva1989.github.io"]
+urlApi()
+
+function urlApi() {
+    arrayHostProd.forEach(element => {
+        if (window.location.host === element) {
+            urlAPIJogos = 'https://jogosempromocoes.azurewebsites.net/api/jogos/'
+        }
+    });
+
+    arrayHostDev.forEach(element => {
+        if (window.location.host === element) {
+            urlAPIJogos = 'https://jogosempromocoesdev.azurewebsites.net/api/jogos/'
+            document.getElementById('ambiente-dev').style.display = 'block'
+        }
+    })
+}
